277b12ce940fb5cc48a030bd3e74c6fe7192e178
ahassan@azavea.com <adeel.hassan93@gmail.com>
2019-07-19 11:50:11 -0400

    add flags to disable timeout and ignore cell errors

--- download_run_upload_jupyter.sh
+++ download_run_upload_jupyter.sh
@@ -5,6 +5,6 @@ NOTEBOOK_NAME=$2
 
 aws s3 cp $NOTEBOOK_LOC $NOTEBOOK_NAME
 
-jupyter nbconvert --execute --to notebook --inplace ./$NOTEBOOK_NAME
+jupyter nbconvert --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True --execute --to notebook --inplace ./$NOTEBOOK_NAME
 
 aws s3 cp $NOTEBOOK_NAME $NOTEBOOK_LOC
